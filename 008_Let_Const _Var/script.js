58d74b8ef8ba445c17b21471376f643cc19e948c
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-13 05:53:03 +0530

    code

---
+++ 008_Let_Const _Var/script.js
@@ -0,0 +1,37 @@
+
+
+/*
+
+Variable - variable keywords are the special keyword that is used to declare a variable and initialise a values.
+
+
+let : let is a variable keyword that is used to declare a variable and initialise a value using let we can not re-declare but we can re-initialise the value.
+
+
+
+const : const is a variable keyword that is used to declare a variable and initialise a value using const we can not re-declare & re-initialise the value.
+
+
+
+var : var is a variable keyword that is used to declare a variable and initialise a value using Var we can declare & re-initialise the value.
+
+*/
+
+
+let age = 30;
+age = 33;
+
+
+
+const birthYear = 1998;
+// birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = "Bisnoi";
+console.log(lastName);
+
+
+
+
---
+++ Value & Variables/script.js
@@ -0,0 +1,34 @@
+let js = "amazing"; //var declare
+console.log(40+8+23-40);
+
+
+console.log('jonas');
+console.log(23);
+
+
+
+
+//variable name conventions.
+
+
+let firstName = "jones"  // declaring variable.
+
+console.log(firstName); 
+//camel cases means when we want to write more than one word that time we use camelCase.
+
+//we can not write let  3years = 3; this is syntax error.
+
+//only two symbol is allowed 
+
+let _name = "Jonathan";
+
+let $firstName = "jones";
+
+
+let PI = 3.1415;
+console.log(PI);  //this is conventional error
+
+
+
+
+
