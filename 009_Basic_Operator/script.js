73caf6e4626aeaf0f424801e601b18acfaed490e
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-13 06:16:15 +0530

    code

--- 009_Basic_Operator/script.js
+++ 009_Basic_Operator/script.js
@@ -19,6 +19,26 @@ console.log(age_lonas * 2, age_lonas/10,2**3);
 //2**3 means 2 to the power of 3 = 2* 2 * 2
 
 
+// Assignment Operator
+
+let x = 10 + 5; //15
+x += 10;        //x = x+10
+x *= 4;         //x = x * 4  = 100
+x ++;
+x--;
+console.log(x);
+
+console.log(x);
+
+
+
+//Comparision Operators.  >, <, >=, <=
+
+console.log((age_sarath > age_lonas));
+
+console.log(age_sarath>=18);
+
+
 
 
 
