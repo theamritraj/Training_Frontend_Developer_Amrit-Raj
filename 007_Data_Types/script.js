796cca626bb90794402181f5c416a4bc15d79762
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-13 05:52:53 +0530

    code

---
+++ 007_Data_Types/script.js
@@ -0,0 +1,77 @@
+/*
+
+Data Types : - types of variable or objects created in javascript are classified using the datatypes.
+
+data Type classified into two ways :-
+
+01. Primitive Data type
+
+1.Number :- number is the one of the primitve data type that is use to store decimal integral and integral values.
+
+ex: let age = 23;
+
+2.Sting :- String is the one of the primitve data type that is use to store sequence of character.
+ex: let firstName = "Jones";
+
+3.Boolean :- Boolean  is the one of the primitve data type that is use to store logical type that can only true or false.(ua=se for taking decission) 
+let fullAge = true;
+
+4.Undefined :- value taken by a variable that is not yet defined ('empty value');
+
+let children;
+
+5. symbol(ES2015) :- value that is unique and can not be changed [not useful for now]
+
+7.BigInt(ES2020) Larger integers than the number type can hold.
+
+02.  Non Primitive Data Type
+
+
+comments in js
+
+1 single line comment.    //
+2.multi line comments.     /*     */ 
+
+
+
+/*
+
+*/
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jones');
+
+
+
+
+
+
+
+//re assign
+javascriptIsFun = 'Yes';
+console.log(javascriptIsFun);
+
+
+//undefined
+var year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+
+// null
+console.log(typeof null);  //object
+
+
+
+
+
+
