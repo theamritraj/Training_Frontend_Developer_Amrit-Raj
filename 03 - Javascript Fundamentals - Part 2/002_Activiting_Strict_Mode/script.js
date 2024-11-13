bee832e2bfd491100a455e76cbcfbc4406072fa2
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-13 16:53:46 +0530

    code

---
+++ 03 - Javascript Fundamentals - Part 2/002_Activiting_Strict_Mode/script.js
@@ -0,0 +1,16 @@
+/*
+
+In JavaScript, `"use strict";` enables strict mode, which enforces stricter error-checking and prevents the use of undeclared variables. It helps catch common mistakes and improves code security. Strict mode can also enable optimizations in some JavaScript engines for better performance.
+
+*/
+
+
+
+'use Strict';
+
+let hasDriverLicennse = false;
+const passTest = true;
+
+if(passTest) hasDriverLicennse = true;
+if(hasDriverLicennse) console.log('i can drive');
+
