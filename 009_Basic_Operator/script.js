08b400cd93f4d84d3ed4f7e4c2923a4a42d86c91
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-13 06:00:40 +0530

    code

---
+++ 009_Basic_Operator/script.js
@@ -0,0 +1,24 @@
+/*
+
+Operators In Js :- it performs basic mathematical operations.
+operators are symbols that are used to perform certain operations.
+
+
+
+1. Arithmetic Operator. + - * / % = 
+
+*/
+
+const now = 2037;
+const age_lonas = now - 1991;
+const age_sarath = now - 2018;
+console.log(age_lonas, age_sarath);
+
+
+console.log(age_lonas * 2, age_lonas/10,2**3);
+//2**3 means 2 to the power of 3 = 2* 2 * 2
+
+
+
+
+
