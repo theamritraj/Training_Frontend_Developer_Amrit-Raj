28d2af7562135bb61477eb2099b0409f82cc5609
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-16 13:57:58 +0530

    code

---
+++ 02 - Javascript Fundamentals - Part 1/024_#CHALLENGE_03/script.js
@@ -0,0 +1,23 @@
+
+
+
+//Challenges  #03
+
+
+
+
+const scoreDolphins = (96 + 108 + 89)/3;
+const scorekolas = (88+91 + 110)/3;
+console.log(scoreDolphins,scoreKolas);
+
+if(scoreDolphins > scorekolas){
+  console.log("dolphins win the trophy ");
+  
+}else if(scoreKolas>scoreDolphins){
+  console.log("Koalas win the trophy ");
+}else if(scoreKolas===scoreDolphins){
+  console.log("both win the trophy ");
+}
+
+
+//Bonus
