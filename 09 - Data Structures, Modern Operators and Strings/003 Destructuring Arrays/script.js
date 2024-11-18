61269aaa40055c07751f2767594bb0cf84e3ec9b
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-18 21:40:50 +0530

    code

---
+++ 09 - Data Structures, Modern Operators and Strings/003 Destructuring Arrays/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const restaurant = {
+  name:'classico Italiano',
+  location : 'via Angelo tavanti 23, Firenze, Italy',
+  categories:['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu : ['Focaccia','Bruschetta','Garlic Bread','Caprese Salad'],
+  mainMenu : ['Pizza','pasta','Risotto'],
+
+};
+
+ const arr = [2,3,4];
+ const a = arr[0];
+ const b = arr[1];
+ const c = arr[2];
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ 
+
+ const [x,y,z] =arr;
+ console.log(x,y,z);
+
+
+ let num = [20,30,40];
+
+ const [d,e,f] = num;
+ console.log(d,e,f);
+ 
+ 
