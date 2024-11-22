c103376525e41f8a56857ecfdffb616dee5e6f9f
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2024-11-22 17:54:42 +0530

    code

--- 11 - Working With Arrays/008 Creating DOM Elements/script.js
+++ 11 - Working With Arrays/008 Creating DOM Elements/script.js
@@ -239,6 +239,24 @@ btnSort.addEventListener('click', function (e) {
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 /////////////////////////////////////////////////
 // Simple Array Methods
